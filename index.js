a8d3c3fa7bad34548960411f39518d2f368768ec
nitaybz <nitaybz@gmail.com>
2017-01-18 00:15:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     Accessory = homebridge.hap.Accessory;
-
+    HomebridgeAPI = homebridge;
     homebridge.registerAccessory('homebridge-tado', 'TADO', TadoAccessory);
 }
 
