8f0adc960bdfd24c471104101a565bdf0d11c376
nitaybz <nitaybz@gmail.com>
2017-01-27 14:23:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -451,9 +451,6 @@ TadoAccessory.prototype._setOverlay = function(body) {
     }
     
     https.request(options, null).end(body);    
-    if (body == null) {
-        accessory.service.setCharacteristic(Characteristic.TargetTemperature, accessory.lastTemp); 
-    }
 }
 
 TadoAccessory.prototype._setTargetCoolingOverlay = function(temp) {
